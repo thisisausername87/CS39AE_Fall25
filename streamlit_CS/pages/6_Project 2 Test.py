1ef4f3f4d7c6b408c52d58549bb3fcb2493a9f02
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-01 14:24:02 -0700

    Update 6_Project 2 Test.py

--- streamlit_CS/pages/6_Project 2 Test.py
+++ streamlit_CS/pages/6_Project 2 Test.py
@@ -21,7 +21,8 @@ fig.update_layout(
     yaxis_title="Count"
 )
 st.plotly_chart(fig)
-st.write("This histogram shows the number of jobs that are in each slary range.")
+st.write("This histogram shows the number of jobs that are in each starting median slary range. The number of jobs in each salary range can be seen by correlating the count value on the left with the blocks of salary ranges at the bottom. More detailed information can be seen by hovering over the blocks.")
+st.write("This data shows that the vast majority of jobs have a starting median salary of below around $50k per year. Some of the most notable data points in the graph are the lowest and highest valued bins, with 3 jobs having a starting salary below $35k per year and only 1 job having a starting salary of above $70k per year. This distribution is heavily right skewed with most jobs having a starting salary in the range of about $35k to $45k.")
 
 fig = px.scatter(df_sorted, x="Starting Median Salary", y="Percent change from Starting to Mid-Career Salary", title="Percent Salary Increase Based on Starting Salary")
 fig.update_layout(

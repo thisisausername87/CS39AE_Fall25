dcc131068d224b9b520bdd2358189e7fb4699458
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-01 14:11:15 -0700

    Update 6_Project 2 Test.py

--- streamlit_CS/pages/6_Project 2 Test.py
+++ streamlit_CS/pages/6_Project 2 Test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-data_path = os.path.join(os.getcwd(), "data", "degrees_that_pay_back.csv")
+data_path = os.path.join(os.getcwd(), "streamlit_CS", "data", "degrees_that_pay_back.csv")
 df = pd.read_csv(data_path)
 df_sorted = df.sort_values(by="Starting Median Salary")
 

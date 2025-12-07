9fce64c03243b739b60fbfc79d53c6b65cc39a8b
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-06 22:06:55 -0700

    Update 6_Lab 6.1.py

--- streamlit_CS/pages/6_Lab 6.1.py
+++ streamlit_CS/pages/6_Lab 6.1.py
@@ -7,7 +7,9 @@ G = nx.Graph()
 G.add_edges_from([("Alice", "Bob"), ("Alice", "Charlie"), ("Bob", "Charlie"), ("Charlie", "Diana"), ("Diana", "Eve"), ("Bob", "Diana"), ("Frank", "Eve"), ("Eve", "Ian"), ("Diana", "Ian"), ("Ian", "Grace"), ("Grace", "Hannah"), ("Hannah", "Jack"), ("Grace", "Jack"), ("Charlie", "Frank"), ("Alice", "Eve"), ("Bob", "Jack")])
 fig, ax = plt.subplots()
 pos = nx.spring_layout(G)
-nx.draw_circular(G, with_labels=True, node_color='lightblue', font_weight='bold')
+special_nodes = {Bob}
+node_colors = ['red' if n in special_nodes else 'lightblue' for n in G.nodes()]
+nx.draw_circular(G, with_labels=True, node_color=node_colors, font_weight='bold')
 st.pyplot(fig)
 
 degree_centrality = nx.degree_centrality(G)

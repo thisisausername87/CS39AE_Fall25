596efa4ef0cbd4ccc87a2b4ce37b3126be1d827b
thisisausername87 <165698057+thisisausername87@users.noreply.github.com>
2025-12-06 21:16:20 -0700

    Create 6_Lab 6.1.py

---
+++ streamlit_CS/pages/6_Lab 6.1.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.algorithms.community import greedy_modularity_communities
+
+G = nx.Graph()
+G.add_edges_from([("Alice", "Bob"), ("Alice", "Charlie"), ("Bob", "Charlie"), ("Charlie", "Diana"), ("Diana", "Eve"), ("Bob", "Diana"), ("Frank", "Eve"), ("Eve", "Ian"), ("Diana", "Ian"), ("Ian", "Grace"), ("Grace", "Hannah"), ("Hannah", "Jack"), ("Grace", "Jack"), ("Charlie", "Frank"), ("Alice", "Eve"), ("Bob", "Jack")])
+pos = nx.spring_layout(G)
+nx.draw_circular(G, with_labels=True, node_color='lightblue', font_weight='bold')
+plt.show()
+
+degree_centrality = nx.degree_centrality(G)
+print("Degree Centrality:")
+for node, score in degree_centrality.items():
+    print(f"{node}: {score}")
+
+print("\n")
+
+betweenness_centrality = nx.betweenness_centrality(G, weight='weight')
+print("Betweenness Centrality:")
+for node, score in betweenness_centrality.items():
+    print(f"{node}: {score}")
+
+print("\n")
+
+closeness_centrality = nx.closeness_centrality(G)
+print("Closeness Centrality:")
+for node, score in closeness_centrality.items():
+    print(f"{node}: {score}")
